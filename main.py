4428bae376f1ccbc49ddfc67725592e5683c5975
Exr0n <mail@exr0n.com>
2022-12-14 23:21:12 -0800

    so um well fruck but its ok its trig time

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # python3 -m pip install solidpython
 from solid import scad_render_to_file
-from solid import cube, sphere, cylinder, hole, translate
-from solid.utils import difference
-from solid.utils import up, down, left, right
+# from solid import cube, sphere, cylinder, hole, translate
+# from solid.utils import difference
+# from solid.utils import up, down, left, right
+
+# python3 -m pip install solidpython2
+from solid2 import cube, cylinder, translate
 
 from math import atan2, pi
 
@@ -16,12 +19,12 @@ HOLE_RADIUS = 0.25
 
 SEGMENTS = 50
 
-farthest_distance = -1 + 1/NUM_NOZZLES
+# farthest_distance = -1 + 1/NUM_NOZZLES
 
 def deg(rad):
     return rad / pi * 180
 
-print(deg(atan2(farthest_distance, HOSE_HEIGHT)))
+# print(deg(atan2(farthest_distance, HOSE_HEIGHT)))
 
 def get_coords(n_nozzles, spray_sidelen):
     assert n_nozzles % 2 == 0
@@ -40,18 +43,29 @@ def get_angles_for_floor_coord(floor_coord, nozzle_cord):
     nx, ny, nz = nozzle_cord
     return (deg(atan2(x-nx, nz-z)), deg(atan2(y-ny, nz-z)))
 
-for floor_coord in get_coords(NUM_NOZZLES, SPRAY_SIDELEN):
-    x, y = get_angles_for_floor_coord(floor_coord, [0, 0, HOSE_HEIGHT])
-    # print(f"{x:.2f} {y:.2f}")
+
+
+if __name__ == '__main__':
+    HEIGHT = 10
+    scad =  cylinder(r=HOLE_RADIUS, h=HEIGHT).down(HEIGHT).rotateY(-45)
+    scad += cylinder(r=HOLE_RADIUS, h=HEIGHT).down(HEIGHT).rotateX(-45)
+    scad += cylinder(r=HOLE_RADIUS, h=HEIGHT).down(HEIGHT).rotateX(-45).rotateY(-45)
+    scad += cylinder(r=HOLE_RADIUS, h=HEIGHT).down(HEIGHT).rotateY(-45).rotateX(-45)
+    scad_render_to_file(scad, 'square_nozzle.scad', file_header=f'$fn = {SEGMENTS};')
+    print('done')
 
 
 
+if __name__ == 'e__main__':
+    for floor_coord in get_coords(NUM_NOZZLES, SPRAY_SIDELEN):
+        x, y = get_angles_for_floor_coord(floor_coord, [0, 0, HOSE_HEIGHT])
+        # print(f"{x:.2f} {y:.2f}")
 
-scad = up(-NOZZLE_DEPTH/2 + HOSE_HEIGHT)(cube([NOZZLE_SIDELEN, NOZZLE_SIDELEN, NOZZLE_DEPTH], center=True))
+    scad = up(-NOZZLE_DEPTH/2 + HOSE_HEIGHT)(cube([NOZZLE_SIDELEN, NOZZLE_SIDELEN, NOZZLE_DEPTH], center=True))
 
-for floor_coord in get_coords(NUM_NOZZLES, SPRAY_SIDELEN):
-    x, y, z = floor_coord
-    scad += translate(floor_coord)(cylinder(r=HOLE_RADIUS*5e1, h=1))
+    for floor_coord in get_coords(NUM_NOZZLES, SPRAY_SIDELEN):
+        x, y, z = floor_coord
+        scad += translate(floor_coord)(cylinder(r=HOLE_RADIUS*5e1, h=1))
 
 # scad -= hole()(translate([0, 0, -1])(cylinder(r=2, h=12)))
-scad_render_to_file(down(HOSE_HEIGHT)(scad), "square_nozzle.scad", file_header=f'$fn = {SEGMENTS};',)
+    scad_render_to_file(down(HOSE_HEIGHT)(scad), "square_nozzle.scad", file_header=f'$fn = {SEGMENTS};',)
