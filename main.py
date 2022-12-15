398b232b3383b5b8bf63282ee7c1f1597abed72d
Exr0n <mail@exr0n.com>
2022-12-14 23:53:14 -0800

    just the holes for linear (no grav)

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from solid import scad_render_to_file
 # from solid.utils import up, down, left, right
 
 # python3 -m pip install solidpython2
-from solid2 import cube, cylinder, translate
+from solid2 import cube, cylinder, translate, sphere
+
+from tqdm import tqdm
 
 from math import atan2, pi
 
@@ -17,7 +19,7 @@ NOZZLE_DEPTH = 5 * 10
 NOZZLE_SIDELEN = 12 * 10
 HOLE_RADIUS = 0.25
 
-SEGMENTS = 50
+SEGMENTS = 100
 
 # farthest_distance = -1 + 1/NUM_NOZZLES
 
@@ -53,29 +55,32 @@ def get_angles_for_floor_coord(floor_coord, nozzle_cord):
 def make_cylinder_from_nozzle_normal(from_x, up, r=HOLE_RADIUS, h=10):
     return cylinder(r=r, h=h).down(h).rotateY(-up).rotateZ(from_x)
 
+# if __name__ == '__main__':
+#     HEIGHT = 10
+#     eangles = get_angles_for_floor_coord([0, -1000, 0], [0, 0, HOSE_HEIGHT])
+#     scad = make_cylinder_from_nozzle_normal(*eangles, h=HEIGHT)
+#     # scad =  cylinder(r=HOLE_RADIUS, h=HEIGHT).down(HEIGHT).rotateY(-90) .rotateY(-45).rotateZ(90)
+#     # scad += cylinder(r=HOLE_RADIUS, h=HEIGHT).down(HEIGHT).rotateX(45)
+#     # scad += cylinder(r=HOLE_RADIUS, h=HEIGHT).down(HEIGHT).rotateX(45).rotateY(45)
+#     # scad += cylinder(r=HOLE_RADIUS, h=HEIGHT).down(HEIGHT).rotateY(45).rotateX(45)
+#     scad_render_to_file(scad, 'square_nozzle.scad', file_header=f'$fn = {SEGMENTS};')
+#     print('done')
+
+
+
 if __name__ == '__main__':
-    HEIGHT = 10
-    eangles = get_angles_for_floor_coord([0, -1000, 0], [0, 0, HOSE_HEIGHT])
-    scad = make_cylinder_from_nozzle_normal(*eangles, h=HEIGHT)
-    # scad =  cylinder(r=HOLE_RADIUS, h=HEIGHT).down(HEIGHT).rotateY(-90) .rotateY(-45).rotateZ(90)
-    # scad += cylinder(r=HOLE_RADIUS, h=HEIGHT).down(HEIGHT).rotateX(45)
-    # scad += cylinder(r=HOLE_RADIUS, h=HEIGHT).down(HEIGHT).rotateX(45).rotateY(45)
-    # scad += cylinder(r=HOLE_RADIUS, h=HEIGHT).down(HEIGHT).rotateY(45).rotateX(45)
-    scad_render_to_file(scad, 'square_nozzle.scad', file_header=f'$fn = {SEGMENTS};')
-    print('done')
-
-
-
-if __name__ == 'e__main__':
     for floor_coord in get_coords(NUM_NOZZLES, SPRAY_SIDELEN):
         x, y = get_angles_for_floor_coord(floor_coord, [0, 0, HOSE_HEIGHT])
         # print(f"{x:.2f} {y:.2f}")
 
-    scad = up(-NOZZLE_DEPTH/2 + HOSE_HEIGHT)(cube([NOZZLE_SIDELEN, NOZZLE_SIDELEN, NOZZLE_DEPTH], center=True))
+    # scad = cube([NOZZLE_SIDELEN, NOZZLE_SIDELEN, NOZZLE_DEPTH], center=True).up(-NOZZLE_DEPTH/2 + HOSE_HEIGHT)
+    scad = sphere(r=1)
 
-    for floor_coord in get_coords(NUM_NOZZLES, SPRAY_SIDELEN):
-        x, y, z = floor_coord
-        scad += translate(floor_coord)(cylinder(r=HOLE_RADIUS*5e1, h=1))
+    for floor_coord in tqdm(get_coords(NUM_NOZZLES, SPRAY_SIDELEN)):
+        # x, y, z = floor_coord
+        eangles = get_angles_for_floor_coord(floor_coord, [0, 0, HOSE_HEIGHT])
+        scad += make_cylinder_from_nozzle_normal(*eangles, h=100)
+        # scad += translate(floor_coord)(cylinder(r=HOLE_RADIUS*5e1, h=1))
 
 # scad -= hole()(translate([0, 0, -1])(cylinder(r=2, h=12)))
-    scad_render_to_file(down(HOSE_HEIGHT)(scad), "square_nozzle.scad", file_header=f'$fn = {SEGMENTS};',)
+    scad_render_to_file(scad, "square_nozzle.scad", file_header=f'$fn = {SEGMENTS};',)
